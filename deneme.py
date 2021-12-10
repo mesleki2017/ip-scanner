a9387034bcbc4c17db53dadc54f193c1fc35e861
mesleki2017 <mesleki2017@gmail.com>
2021-12-10 13:04:16 +0300

    yanlıis oldu silmem laazim hepsini

--- deneme.py
+++
@@ -1,9 +0,0 @@
-import ip_scanner 
-from ip_scanner1 import ip_scan as ipscan11
-
-#print(ipscan11(140,160))
-print("---------------------------------")
-
-aaa= ip_scanner.scan()
-ffff=aaa.ip_scan(140,160)
-print(ffff)
